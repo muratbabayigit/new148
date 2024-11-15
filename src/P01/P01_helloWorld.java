cf798d70dde78a9c6ee20b23fe5b6cb9bfd7ac68
babayigit <babayigit@gmail.com>
2024-11-15 11:40:22 +0300

    first commit

---
+++ src/P01/P01_helloWorld.java
@@ -0,0 +1,7 @@
+package P01;
+
+public class P01_helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/P02/p01.java
@@ -0,0 +1,9 @@
+package P02;
+
+public class p01 {
+    public static void main(String[] args) {
+        int a=128;
+        int b=17;
+        System.out.println("c="+(a+b));
+    }
+}
