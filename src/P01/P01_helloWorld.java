7d5f4ffffd8d3fb541c85feb042a8ff65bb73dcc
babayigit <babayigit@gmail.com>
2024-11-15 11:44:37 +0300

    second commit

--- src/P01/P01_helloWorld.java
+++ src/P01/P01_helloWorld.java
@@ -3,5 +3,6 @@ package P01;
 public class P01_helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Github");
     }
 }
--- src/P02/p01.java
+++ src/P02/p01.java
@@ -1,9 +1,20 @@
 package P02;
 
+import java.util.Scanner;
+
 public class p01 {
     public static void main(String[] args) {
         int a=128;
         int b=17;
         System.out.println("c="+(a+b));
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Bir sayı giriniz");
+        int sayi= scan.nextInt();
+        if (sayi%2==0){
+            System.out.println("Girdiğiniz sayı çift sayıdır");
+        }else{
+            System.out.println("Girdiğiniz sayı çift sayı değildir");
+        }
     }
 }
